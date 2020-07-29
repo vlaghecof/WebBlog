b1ec396bfd1cec4e8136170cbf813442c98648fe
vlaghecof <vladcofaru@gmail.com>
2020-07-29 19:17:17 +0300

    Developed the user profile

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,9 @@ from django.contrib.auth import views as auth_views
 from django.urls import path,include
 from users import views as user_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
@@ -25,4 +28,9 @@ urlpatterns = [
     path('profile/',user_views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
