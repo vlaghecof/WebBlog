5e056ed6faa0349957e44aa15012729eb0077cc0
vlaghecof <vladcofaru@gmail.com>
2020-08-04 13:07:59 +0300

    Fully functional site

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,9 +125,14 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
 LOGIN_REDIRECT_URL='blog-home'
-
 LOGIN_URL='login'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-
 MEDIA_URL='/media/'
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT= 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER="automateemailtestcv@gmail.com"
+EMAIL_HOST_PASSWORD="cofaru123"
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateform
 from django.contrib.auth.decorators import login_required #to make sure a link is accesible only if you are logged in
+
 
 def register(request):
     if request.method=='POST':
@@ -18,4 +19,21 @@ def register(request):
 
 @login_required #to ensure only logged in users can see a view
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method=='POST':
+        u_form=UserUpdateForm(request.POST,instance=request.user) #to populate the forms with the curent data
+        p_form=ProfileUpdateform(request.POST,request.FILES,instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)  # to populate the forms with the curent data
+        p_form = ProfileUpdateform(instance=request.user.profile)
+
+    contex={
+        'u_form':u_form,
+        'p_form':p_form
+    }
+
+    return render(request,'users/profile.html',contex)
